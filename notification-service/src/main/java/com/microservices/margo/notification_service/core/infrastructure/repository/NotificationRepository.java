5135ffc3bb5600963cf01eca665e580b6fd8cc03
Marharyta Filipovych <marharyta.filipovych@cantire.com>
2026-03-10 21:06:55 +0200

    repository annotation

--- notification-service/src/main/java/com/microservices/margo/notification_service/core/infrastructure/repository/NotificationRepository.java
+++ notification-service/src/main/java/com/microservices/margo/notification_service/core/infrastructure/repository/NotificationRepository.java
@@ -2,9 +2,11 @@ package com.microservices.margo.notification_service.core.infrastructure.reposit
 
 import com.microservices.margo.notification_service.core.infrastructure.entity.NotificationEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @SuppressWarnings("NullableProblems")
+@Repository
 public interface NotificationRepository extends JpaRepository<NotificationEntity, UUID> {}
 
